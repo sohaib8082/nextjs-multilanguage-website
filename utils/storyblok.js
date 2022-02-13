7184b5051e7f5790abec4ff23162db89eb03d67a
sohaib8082 <87477136+sohaib8082@users.noreply.github.com>
2022-02-13 15:26:05 +0500

    Update storyblok.js

--- utils/storyblok.js
+++ utils/storyblok.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import StoryblokClient from "storyblok-js-client";
 
 const Storyblok = new StoryblokClient({
-  accessToken: "YOUR_PREVIEW_TOKEN",
+  accessToken: "6agNBU2wr9Gch4hGnnny7gtt",
   cache: {
     clear: "auto",
     type: "memory",
